652f22b634a0a035728ba77a4d4ecfe8556a7529
Jan Stavěl <stavel.jan@gmail.com>
2013-10-18 11:52:01 +0200

    added interface for Response and Handler and Sender.

--- edeposit/amqp/interfaces.py
+++ edeposit/amqp/interfaces.py
@@ -5,7 +5,7 @@ _ = gettext.gettext
 
 class IResponse(interface.Interface):
     uuid = schema.ASCIILine(
-        title = _(u'UUID of a request'),
+        title = _(u'UUID of a Response'),
         description = _(u'Unique number of a response message'),
         required = True,
         )
@@ -42,3 +42,51 @@ class IResponse(interface.Interface):
         )
 
 
+class IRequest(interface.Interface):
+    uuid = schema.ASCIILine(
+        title = _(u'UUID of a request'),
+        description = _(u'Unique number of a request message'),
+        required = True,
+        )
+    
+    created = schema.Datetime(
+        title = _(u'Date time when Request was created at.'),
+        description = _(u''),
+        required = True,
+        readonly = False,
+        )
+    
+    interfaceOfMsg = schema.ASCIILine(
+        title = _(u'Interface describing msg attribute'),
+        description = _(u'Request message wears inner msg (see attribute msg). The msg has its own interface.'),
+        required = False,
+        )
+
+    serializationOfMsg = schema.ASCIILine(
+        title = _(u'Serialization interface'),
+        description = _(u'What way was msg attribute serialized.'),
+        required = True,
+        )
+    
+    msg = schema.Bytes(
+        title = _(u'Inner message'),
+        description = _(u'Message that wears main information for consuments.'),
+        required = True,
+        )
+    
+
+
+class IRequestHandler(interface.Interface):
+    request = interface.Attribute("request to be handled")
+
+    def handle():
+        """ it will do something with request message. It returns new message with result of an handler.
+        The result will be sent by other interface.
+        """
+
+class IResponseSender(interface.Interface):
+    response = interface.Attribute("response to be sent")
+    def send():
+        """ the response will be sent """
+        
+    
