013362a62b68b1c27835fabb376803aae9c67d58
Bystroushaak <bystrousak@kitakitsune.org>
2014-02-18 14:13:51 +0100

    Pacakge requirements in setup.py updated.
    Also refactored little bit to comply with PEP8 standard.

--- setup.py
+++ setup.py
@@ -2,32 +2,37 @@ from setuptools import setup, find_packages
 import os
 
 version = '0.9'
+description = open("README.txt").read() + "\n"
+description += open(os.path.join("docs", "HISTORY.txt")).read()
 
-setup(name='edeposit.amqp',
-      version=version,
-      description="E-Deposit AMQP Common Package",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='edeposit.amqp',
+    version=version,
+    description="E-Deposit AMQP Common Package",
+    long_description=description,
+    url='https://github.com/jstavel/edeposit.amqp/',
+
+    author='Edeposit team',
+    author_email='',
+
+    classifiers=[
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='https://github.com/jstavel/edeposit.amqp/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['edeposit'],
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='edeposit.amqp.tests',
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    ],
+
+    keywords='',
+    license='GPL',
+
+    packages=find_packages(exclude=['ez_setup']),
+
+    namespace_packages=['edeposit'],
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='edeposit.amqp.tests',
+    install_requires=[
+        'setuptools',
+        "python-daemon>=1.5.5"
+    ],
+    # entry_points="""
+    # # -*- Entry points: -*-
+    # """,
+)
