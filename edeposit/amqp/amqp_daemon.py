ae35c58187305d5d3f40dfc46bb071a14f30b804
Bystroushaak <bystrousak@kitakitsune.org>
2014-02-19 11:01:48 +0100

    Basic daemon template extended, it now looks like this should work.

--- edeposit/amqp/amqp_daemon.py
+++ edeposit/amqp/amqp_daemon.py
@@ -4,14 +4,23 @@
 # Interpreter version: python 2.7
 #
 #= Imports ====================================================================
+import sys
 import time
-from daemon import runner  # python-daemon package from pypi
+from string import Template
+
+
+import pika
+import daemon  # python-daemon package from pypi
+from daemon import runner
 
 
 #= Variables ==================================================================
+SERVER_TEMPLATE = "amqp://$USERNAME:$PASSWORD@$SERVER:$PORT/%2f"  # %2f = /
 
 
 #= Functions & objects ========================================================
+
+
 class App():
     def __init__(self):
         self.stdin_path = '/dev/null'
@@ -20,16 +29,48 @@ class App():
         self.pidfile_path = '/tmp/foo.pid'
         self.pidfile_timeout = 5
 
+        if self.isRunning() and "stop" not in sys.argv:
+            print 'It looks like a daemon is already running!'
+            exit()
+
+    def start(self):
+        try:
+            daemon_runner = runner.DaemonRunner(self)
+            daemon_runner.do_action()
+        except daemon.runner.DaemonRunnerStopFailureError:
+            print "There is nothing to kill."
+            sys.exit(0)
+        except SystemExit:
+            self.at_exit()
+
     def run(self):
         while True:
             print("Howdy!  Gig'em!  Whoop!")
             time.sleep(10)
 
-app = App()
-daemon_runner = runner.DaemonRunner(app)
-daemon_runner.do_action()
+        self.at_exit()  # this is run only if daemon was not started properly
+
+    def at_exit(self):
+        print "exiting"
+
+    def isRunning(self):
+        lockfile = runner.make_pidlockfile(self.pidfile_path, 1)
+
+        return lockfile.is_locked()
 
 
 #= Main program ===============================================================
 if __name__ == '__main__':
-    pass
+    a = App()
+    a.start()
+
+    # connection = pika.BlockingConnection(
+    #     pika.URLParameters(
+    #         Template(SERVER_TEMPLATE).substitute(
+    #             USERNAME=settings.RABBITMQ_USER_NAME,
+    #             PASSWORD=settings.RABBITMQ_USER_PASSWORD,
+    #             SERVER=settings.RABBITMQ_HOST,
+    #             PORT=settings.RABBITMQ_PORT
+    #         )
+    #     )
+    # )
