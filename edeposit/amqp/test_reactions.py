ae654ca46817915146945c451df66758022eb4a0
Bystroushaak <bystrousak@kitakitsune.org>
2014-02-21 12:55:00 +0100

    Script for testing communication updated.

--- edeposit/amqp/test_reactions.py
+++ edeposit/amqp/test_reactions.py
@@ -2,21 +2,53 @@
 # -*- coding: utf-8 -*-
 #
 # Interpreter version: python 2.7
-# This work is licensed under a Creative Commons 3.0 Unported License
-# (http://creativecommons.org/licenses/by/3.0/).
 #
 #= Imports ====================================================================
 import aleph
 import aleph.convertors
+
+import settings
+
+import pika
 
 
 #= Main program ===============================================================
 if __name__ == '__main__':
     isbnq = aleph.ISBNQuery("80-251-0225-4")
     request = aleph.SearchRequest(isbnq, "trololo")
+    json_data = aleph.convertors.toJSON(request)
 
-    json = aleph.convertors.toJSON(request)
+    connection = pika.BlockingConnection(  # set connection details
+        pika.ConnectionParameters(
+            host=settings.RABBITMQ_HOST,
+            port=int(settings.RABBITMQ_PORT),
+            virtual_host=settings.RABBITMQ_ALEPH_VIRTUALHOST,
+            credentials=pika.PlainCredentials(
+                settings.RABBITMQ_USER_NAME,
+                settings.RABBITMQ_USER_PASSWORD
+            )
+        )
+    )
+    channel = connection.channel()
 
-    aleph.convertors.fromJSON(json)
+    properties = pika.BasicProperties(
+        content_type="application/json",
+        delivery_mode=1
+    )
 
-    print json
+    channel.basic_publish(
+        exchange=settings.RABBITMQ_ALEPH_EXCHANGE,
+        routing_key=settings.RABBITMQ_ALEPH_DAEMON_KEY,
+        properties=properties,
+        body=json_data
+    )
+
+    for method_frame, properties, body in channel.consume(settings.RABBITMQ_ALEPH_PLONE_QUEUE):
+        print "Message:"
+        print method_frame
+        print properties
+        print body
+        print "---"
+        print
+
+        channel.basic_ack(method_frame.delivery_tag)
