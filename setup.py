aa8e7cc8c519686e8b420c2b4361d4b599c48c38
Bystroushaak <bystrousak@kitakitsune.org>
2014-02-26 14:42:49 +0100

    setup.py updated to be ready for publication at PYPI.
    Integrated README.rst, CONTRIBUTORS.txt and CHANGES.txt into package
    description at PYPI.
    
    Changed description.

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
-import os
 from setuptools import setup, find_packages
 
-version = '0.9'
-description = open("README.txt").read() + "\n"
-description += open(os.path.join("docs", "HISTORY.txt")).read()
+version = '1.1.0'
+long_description = "\n\n".join([
+    open('README.rst').read(),
+    open('CONTRIBUTORS.txt').read(),
+    open('CHANGES.txt').read()
+])
 
 setup(
     name='edeposit.amqp',
     version=version,
-    description="E-Deposit AMQP Common Package",
-    long_description=description,
+    description="E-Deposit' AMQP definitions and common classes/patterns.",
+    long_description=long_description,
     url='https://github.com/jstavel/edeposit.amqp/',
 
     author='Edeposit team',
-    author_email='',
+    author_email='edeposit@email.cz',
 
     classifiers=[
         "Programming Language :: Python :: 2.7",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)"
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
 
-    # keywords='',
     license='GPL2+',
 
     packages=find_packages(exclude=['ez_setup']),
