65d1028a16a9815e519316973603550c53bce1c4
Bystroushaak <bystrousak@kitakitsune.org>
2014-06-13 10:15:48 +0200

    Added two functions - create_blocking_connection() and receive().
    Bump #24

--- bin/edeposit_amqp_tool.py
+++ bin/edeposit_amqp_tool.py
@@ -4,7 +4,19 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+import pika
 
+# if the module wasn't yet installed at this system, load it from package
+try:
+    from edeposit.amqp.settings import *
+except ImportError:
+    sys.path.insert(0, os.path.abspath('../edeposit/'))
+    import amqp
+    sys.modules["edeposit.amqp"] = amqp
+
+
+from edeposit.amqp import settings
+from edeposit.amqp import amqpdaemon
 
 
 # Variables ===================================================================
@@ -12,7 +24,35 @@
 
 
 # Functions & objects =========================================================
+def create_blocking_connection(host):
+    """
+    Return properly created blocking connection.
 
+    Args:
+        host (str): Host as it is defined in :func:`.get_amqp_settings`.
+
+    Uses :func:`edeposit.amqp.amqpdaemon.getConParams`.
+    """
+    return pika.BlockingConnection(
+        amqpdaemon.getConParams(
+            settings.get_amqp_settings()[host.lower()]["vhost"]
+        )
+    )
+
+
+def receive(channel, queue):
+    """
+    Print all messages in queue.
+    """
+    for method_frame, properties, body in channel.consume(queue):
+        print "Message:"
+        print method_frame
+        print properties
+        print body
+        print "---"
+        print
+
+        channel.basic_ack(method_frame.delivery_tag)
 
 
 # Main program ================================================================
