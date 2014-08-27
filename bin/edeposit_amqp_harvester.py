4efc0a40098eb632a0ee13c79ef239ef745a8978
Bystroushaak <bystrousak@kitakitsune.org>
2014-08-27 21:05:35 +0200

    Added edeposit_amqp_harvester.py script.

---
+++ bin/edeposit_amqp_harvester.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import pika
+from pika.exceptions import ConnectionClosed
+
+import edeposit.amqp.harvester
+
+
+# Variables ===================================================================
+
+
+
+# Functions & objects =========================================================
+
+
+
+# Main program ================================================================
+if __name__ == '__main__':
+    pass
